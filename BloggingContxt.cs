a14875357f6b90c2bf1cf688607d119d3dee3f39
Your Name <you@example.com>
2018-01-17 22:07:48 +0100

    inital

---
+++ BloggingContxt.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    namespace Intro
+    {
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Anchor> Roots { get; set; }
+            public DbSet<Node> Nodes { get; set; }
+            
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+            }
+        }
+
+        public class Anchor
+        {
+            public string Name { get; set }
+            public ICollection<RootNode> Properties { get; set; }
+        }
+
+        public class RootNode : Node { }
+
+        public class Node
+        {
+            public string Name { get; set; }
+            public Anchor Root { get; set; }
+            public ICollection<Node> Children { get; set; }
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Intro;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                if (!db.Roots.Any())
+                {
+                    var anchor = new Anchor { Name = "Screen1" };
+                    var rootNode = new RootNode { Name = "Root" };
+                    rootNode.Children.Add(new Node { Name = "Leaf" });
+                    anchor.Properties.Add(rootNode);
+                }
+
+                db.SaveChanges();
+            }
+            using (var db = new BloggingContext())
+            {
+                var anchor = db.Roots.First();
+                anchor.Properties.ToList();
+                anchor.Properties.
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
